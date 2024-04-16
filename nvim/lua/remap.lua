1e8fe41fbc867af272242ea93a523ac8ae6cfb39
FoxFot <72085113+FoxFot@users.noreply.github.com>
2024-04-16 14:26:58 +0300

    Add files via upload

---
+++ nvim/lua/remap.lua
@@ -0,0 +1,7 @@
+vim.g.leader = '\\'
+
+local tscope = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', tscope.find_files, {})
+
+vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
+vim.keymap.set('n', 'yy', vim.cmd.Yank)
